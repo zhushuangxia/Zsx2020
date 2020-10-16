a21f743c5c51df8ad00e60843e6be4091fb5b23b
guaguaq <2388581400@qq.com>
2020-10-16 15:29:09 +0800

    第三次作业

---
+++ M2/app/src/main/java/com/example/m2/NineActivity.java
@@ -0,0 +1,15 @@
+package com.example.m2;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nine);
+    }
+}
